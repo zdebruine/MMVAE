9177613d330405010446d250b0b70b8f334c0767
kinardc <kinardchase@outlook.com>
2023-12-01 12:26:33 -0500

    updated sample info

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from hello_world import __version__
 
 
 setup(
-    name="d-mmvae",
+    name="D-MMVAE",
     description="A research project on diagonal mixture-of-experts variational autoencoding (D-MMVAE).",
     version=__version__,
-    url="https://github.com/zdebruine/D-MMVAE.git",
+    url="https://github.com/zdebruine/D-MMVAE",
     author="GVSU Applied Computing Institute",
     py_modules=[ "hello_world" ]
 )
