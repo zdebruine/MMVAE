1f914a83d2e1ed8af6ee2fdd7750df0960769280
kinardc <kinardchase@outlook.com>
2023-12-01 13:24:49 -0500

    updated setup for multi-file package

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from setuptools import setup
-from hello_world import __version__
+from setuptools import find_packages, setup
 
 
 
 setup(
     name="D-MMVAE",
     description="A research project on diagonal mixture-of-experts variational autoencoding (D-MMVAE).",
-    version=__version__,
+    version="0.1.0.dev0",
     url="https://github.com/zdebruine/D-MMVAE",
     author="GVSU Applied Computing Institute",
-    py_modules=[ "hello_world" ]
+    packages=find_packages()
 )
