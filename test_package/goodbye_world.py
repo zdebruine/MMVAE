28e80a4048e30444a6f12da9a9d4919c517b321f
kinardc <kinardchase@outlook.com>
2023-12-01 14:02:31 -0500

    removed test files

--- test_package/goodbye_world.py
+++
@@ -1,5 +0,0 @@
-def goodbye_world():
-    print( "Goodbye, world! I've been a pip package!" )
-
-if __name__ == "__main__":
-    goodbye_world()
--- test_package/hello_world.py
+++
@@ -1,5 +0,0 @@
-def hello_world():
-    print( "Hello, world! I'm in a pip package!" )
-
-if __name__ == "__main__":
-    hello_world()
