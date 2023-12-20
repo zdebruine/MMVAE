ef50f2cef7a79f452fdac0f998fb1a42ab1693d9
Anthony Boos <tony@TonyRoot.local>
2023-12-20 14:36:35 -0500

    Extended template to include support for multiple modalities

--- src/Models.py
+++ src/Models.py
@@ -1,109 +1,172 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 
-class Encoder(nn.Module):
-    def __init__(self, input_dim, hidden_dims, latent_dim) -> None:
-        super(Encoder, self).__init__()
+class MultiClassDiscriminator(nn.Module):
+    """
+    The Multi-Class Discriminator class is designed to be used by the MMVAE to discourage bias
+    for any one expert in the the shared latent space.
+
+    Args:
+        layers: Designed for an nn.Sequential stack of layers, but can be any object that is callable and returns a Tensor
+                The output size should match the number of classes, and the output must be raw prediction scores
+                PyTorch's CrossEntropy function expects unnormalized logits as input
+
+        classes: List of the expert names, used in getting the proper label that will then be passed to the loss function
+
+    Vars:
+        labels: NxN "Identity Matrix" (tensor) that corresponds to each classes expected output | N = len(classes) 
+    """
+    def __init__(self, layers: nn.Sequential, classes: list[str]) -> None:
+        super(MultiClassDiscriminator, self).__init__()
+        self.layers = layers
+        self.labels = torch.eye(len(classes))
+        self.classes = {}
+        # Classes maps the expert names to an index value to index the labels
+        for i, label in enumerate(classes):
+            self.classes.update({label:i})
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.layers(x)
+    
+    def get_label(self, key: str) -> Tensor:
+        """
+        Retrive the expected/target prediction based on the current expert
+        """
+        return self.labels[self.classes[key]]
+    
+class Expert(nn.Module):
+    """
+    The Expert class is designed to be used by the MMVAE to handle the non-shared portions
+    of the overall network. The Encoder, Decoder, and expert-specific Discriminator.
+
+    Args:
+        encoder: Designed for an nn.Sequential stack of layers, but can be any object that is callable and returns a Tensor
+                 The first stage of the MMVAE forward pass and is expert-specific, the output size of the encoder should 
+                 match the input size of the shared space and match across all experts
+
+        decoder: Designed for an nn.Sequential stack of layers, but can be any object that is callable and returns a Tensor
+                 The last stage of the MMVAE forward pass and is expert-specific, the input size of the decoder should 
+                 match the output size of the shared space and match across all experts
+
+        discriminator: Designed for an nn.Sequential stack of layers, but can be any object that is callable and returns a Tensor
+                       Should output a binary classification on if the data is real or fake
+
+        expert_name: The name of the expert/modality
+    """
+    def __init__(self, encoder: nn.Sequential, decoder: nn.Sequential, discriminator: nn.Sequential, expert_name: str) -> None:
+        super(Expert, self).__init__()
+        self.encoder = encoder
+        self.decorder = decoder
+        self.discriminator = discriminator
+        self.label = expert_name
+
+    def forward_encode(self, x: Tensor) -> Tensor:
+        return self.encoder(x)
+    
+    def forward_decode(self, x: Tensor) -> Tensor:
+        return self.decorder(x)
+    
+    def get_adversarial_feedback(self, x: Tensor) -> Tensor:
+        return self.discriminator(x)
+
+    def __str__(self) -> str:
+        return self.label
+
+class MMVAE(nn.Module):
+    """
+    The MMVAE class is designed to implement a shared VAE that is fed input from multiple-modalities.
+
+    Args:
+        encoder: Designed for an nn.Sequential stack of layers, but can be any object that is callable and returns a Tensor
+                 Encoder portion of the shared space. The input size should match the output of the expert-specific encoder
+                 networks. The output of the encoder should match the input size of the Multi-Class Discriminator and the
+                 input size of the learned distributions in the latent space.
+
+        decoder: Designed for an nn.Sequential stack of layers, but can be any object that is callable and returns a Tensor
+                 Decoder portion of the shared space. The output size should match the input of the expert-specific decoder
+                 networks. The input of the decoder should match the output size of the learned distributions in the latent space.
+
+        mean: Single nn.Linear layer that represents the mean of the learned distribution in the latent space. Should be of size
+              NxM where N is the output size of the Encoder and M is the latent dimension
+
+        var: Single nn.Linear layer that represents the variance of the learned distribution in the latent space. Should be of size
+              NxM where N is the output size of the Encoder and M is the latent dimension
         
-        self.encoder_layers = nn.ModuleList([nn.Linear(input_dim, hidden_dims)])
-        # if len(hidden_dims) > 1:
-        #     for i in range(len(hidden_dims)-1):
-        #         self.encoder_layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
-        self.mean = nn.Linear(hidden_dims, latent_dim)
-        self.var = nn.Linear(hidden_dims, latent_dim)
+        experts: List of Expert objects, one for each individual modality/expert  
+            
+        discriminator: Instance of a Multi-Class Discriminator
 
-    def forward(self, x):
-        for layer in self.encoder_layers:
-            #print("Encoder", x)
-            x = torch.relu(layer(x))
-        return self.mean(x), self.var(x)
+    Vars:
+        current_expert: Int representing the index of the current expert that is being trained. Control is designed to be handled
+                        outside of the Model class in the Trainer class instead as that pairs with the dataloading for each expert
+    """
+    def __init__(self, encoder: nn.Sequential, decoder: nn.Sequential, mean: nn.Linear,
+                 var: nn.Linear, experts: list[Expert], discriminator: MultiClassDiscriminator) -> None:
+        super(MMVAE, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.mean = mean
+        self.var = var
+        self.experts = experts
+        self.discriminator = discriminator
+        self.current_expert = 0
 
-class Decoder(nn.Module):
-    def __init__(self, latent_dim, hidden_dims, output_dim) -> None:
-        super(Decoder, self).__init__()
-        self.decoder_layers = nn.ModuleList([nn.Linear(latent_dim, hidden_dims)])
-        # if len(hidden_dims) > 1:
-        #     for i in range(len(hidden_dims)-1):
-        #         self.decoder_layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
-        self.output = nn.Linear(hidden_dims, output_dim)
-
-    def forward(self, x):
-        for layer in self.decoder_layers:
-            #print("Decoder", x)
-            x = torch.relu(layer(x))
-        return torch.relu(self.output(x))
-
-class VAE(nn.Module):
-    def __init__(self, Encoder, Decoder) -> None:
-        super(VAE, self).__init__()
-        self.Encoder = Encoder
-        self.Decoder = Decoder
-        
-    def reparameterize(self, mean, var):
-        #print("Mean", mean, "Var", var)
-        eps = torch.randn_like(var)#.to(DEVICE)
+    def reparameterize(self, mean: Tensor, var: Tensor) -> Tensor:
+        eps = torch.randn_like(var)
         return mean + var*eps
 
-    def forward(self, x):
-        mean, logvar = self.Encoder(x)
-        z = self.reparameterize(mean, torch.exp(0.5 * logvar))
-        x_hat = self.Decoder(z)
-        return x_hat, mean, logvar
+    def forward(self, x: Tensor) -> tuple[Tensor]:
+        """
+        Implements the complex forward pass of the network. Feeds through the expert-specific encoder,
+        the shared space encoder, the multi-class discriminator and shared latent space, the shared space
+        decoder, the expert-specific decoder, and finally the expert-specific discriminator.
 
+        Returns:
+            x: Generated output of the model for the given expert
 
+            mu: Mean vector of the latent distribution
 
-"""
-def loss_function(x, x_hat, mean, logvar):
-    #reconstruction_loss = nn.functional.binary_cross_entropy(x_hat, x.to_dense(), reduction='sum')
-    #reconstruction_loss = nn.functional.mse_loss(x_hat, x.to_dense())
-    reconstruction_loss = nn.functional.cross_entropy(x_hat, x.to_dense(), reduction='sum')
-    KLD = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
-    return reconstruction_loss + KLD
+            var: Variance vector of the latent distribution
 
+            adv_feedback: Mulit-Class adversarial feedback from the encoder portion
 
+            exp_feedback: Expert discriminator output on generated data
+        """
+        x = self.experts[self.current_expert].forward_encode(x)
+        x = self.encoder(x)
+        adv_feedback = self.discriminator(x)
+        mu = self.mean(x)
+        var = self.var(x)
+        x = self.reparameterize(mu, torch.exp(0.5 * var))
+        x = self.decoder(x)
+        x = self.experts[self.current_expert].forward_decode(x)
+        exp_feedback = self.experts[self.current_expert].get_adversarial_feedback(x)
+        return x, mu, var, adv_feedback, exp_feedback
+    
+    def set_current_expert(self, new_expert: int) -> None:
+        self.current_expert = new_expert
 
-def collate_fn(batch):
-    #Custom collate function to return a list of tensors as a batch rather than one big tensor
-    return batch
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
-print(DEVICE)
-BATCH_SIZE = 100
-dataset = CellxGeneDataset(BATCH_SIZE)
-#dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
-print("Building Encoder...")
-encode = Encoder(60664, 512, 128)
-print("Success!\nBuilding Decoder...")
-decode = Decoder(128, 512, 60664)
-print("Success!\nBuilding model...")
-
-test = VAE(encode, decode).to(DEVICE)
-print("Success!")
-optimizer = Adam(test.parameters())
-#print("Wrapping for multi-GPU")
-#test = torch.nn.DataParallel(test, device_ids=[0,1])
-#test.to(DEVICE)
-#print("Scucess!")
-test.train()
-for epoch in range(30):
-    epoch_time = time.time()
-    total_loss = 0
-    for batch_idx, x in enumerate(dataset):
-        train_loss = 0
-        # print(f'Batch: {batch_idx}, Epoch: {epoch}')
-        optimizer.zero_grad()
-        x = x.to(DEVICE)
-        # x = x.transpose(0, 1)
-
-        x_hat, mean, log_var = test(x)
-        loss = loss_function(x, x_hat, mean, log_var)
+class VAE(nn.Module):
+    """
+    The VAE class is a single expert/modality implementation. It's a simpler version of the
+    MMVAE and functions almost indentically.
+    """
+    def __init__(self, encoder: nn.Sequential, decoder: nn.Sequential, mean: nn.Linear, var: nn.Linear,) -> None:
+        super(VAE, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.mean = mean
+        self.var = var
         
-        train_loss += loss.item()
-        total_loss += train_loss
-        
-        loss.backward()
-        optimizer.step()
-        #print("Batch", batch_idx+1, "Loss:", train_loss / x.shape[0])
-    print("Time to run:", time.time() - epoch_time)
-    print("Epoch", epoch + 1, "complete!", "Average Loss: ", train_loss / (batch_idx*BATCH_SIZE))
-"""
+    def reparameterize(self, mean: Tensor, var: Tensor) -> Tensor:
+        eps = torch.randn_like(var)
+        return mean + var*eps
+
+    def forward(self, x: Tensor) -> tuple[Tensor]:
+        x = self.encoder(x)
+        mu = self.mean(x)
+        var = self.var(x)
+        z = self.reparameterize(mu, torch.exp(0.5 * var))
+        x_hat = self.decoder(z)
+        return x_hat, mu, var
