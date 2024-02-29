c1d6cea03a34a553b0c422d42469904b2b0d11b9
Cardell Taylor <42698413+CTaylah@users.noreply.github.com>
2024-02-29 13:33:27 -0500

    Update gan_main.py

--- gan_testing/gan_main.py
+++ gan_testing/gan_main.py
@@ -4,21 +4,16 @@ from discriminators import annealing
 
 def main(device):
     batch_size = 32
-    num_iterations = 200
-    annealer = annealing.CyclicalAnnealer(0.4, 3.2, num_iterations, 5, 0.5)
-    writer = torch.utils.tensorboard.SummaryWriter('./logs/annealer_test/')
-    for i in range(num_iterations):
-        writer.add_scalar('Annealing', annealer(i), i)
 
-    # trainer = HumanVAE_gan.HumanVAETrainer(
-    #     batch_size,
-    #     device,
-    #     log_dir="/active/debruinz_project/cardell_taylor/logs/VAE_GAN_2"
-    # )
+    trainer = HumanVAE_gan.HumanVAETrainer(
+         batch_size,
+         device,
+         log_dir="/active/debruinz_project/cardell_taylor/logs/VAE_GAN_2"
+     )
 
-    # print("done")
+    print("done")
 
-    # trainer.train(epochs=1)
+    trainer.train(epochs=1)
 
 if __name__ == "__main__":
     CUDA = True
