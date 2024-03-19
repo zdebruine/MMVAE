d0bafdba8b391eccb45d8cabad2b6cebdf0ce60b
kylewscott <kylewscott512@gmail.com>
2024-03-19 00:44:38 -0400

    Most recent model, trainer, and main

--- mmvae/models/Arch_Model.py
+++ mmvae/models/Arch_Model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import mmvae.models.utils as utils
 
 class VAE(nn.Module):
     def __init__(self):
@@ -7,27 +8,38 @@ class VAE(nn.Module):
         # Encoder
         self.encoder = nn.Sequential(
             nn.Linear(60664, 1024),
-            nn.LeakyReLU(),
+            nn.ReLU(),
+            nn.BatchNorm1d(1024, 0.8),
             nn.Linear(1024, 768),
-            nn.LeakyReLU(),
+            nn.ReLU(),
+            nn.BatchNorm1d(768, 0.8),
             nn.Linear(768, 256),
-            nn.LeakyReLU(),
+            nn.ReLU(),
+            nn.BatchNorm1d(256, 0.8)
         )
+        
         self.fc_mu = nn.Linear(256, 128)
         self.fc_var = nn.Linear(256, 128)
         
         # Decoder
         self.decoder = nn.Sequential(
             nn.Linear(128, 256),
-            nn.LeakyReLU(),
+            nn.ReLU(),
+            nn.BatchNorm1d(256, 0.8),
             nn.Linear(256, 768),
-            nn.LeakyReLU(),
+            nn.ReLU(),
+            nn.BatchNorm1d(768, 0.8),
             nn.Linear(768, 1024),
-            nn.LeakyReLU(),
+            nn.ReLU(),
+            nn.BatchNorm1d(1024, 0.8),
             nn.Linear(1024, 60664),
-            nn.LeakyReLU()
-    
+            nn.Sigmoid(),
         )
+
+        utils._submodules_init_weights_xavier_uniform_(self.encoder)
+        utils._submodules_init_weights_xavier_uniform_(self.decoder)
+        utils._submodules_init_weights_xavier_uniform_(self.fc_mu)
+        utils._xavier_uniform_(self.fc_var, -1.0)
 
     def encode(self, x):
         x = self.encoder(x)
