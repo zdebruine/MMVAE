c6a132b2285693ad1c2f11a6104f4dd08f0223ac
Cardell Taylor <cardell.taylor94@gmail.com>
2024-03-29 19:26:35 -0400

    added __log_hyperparameters to HPTraininer, renamed some variables, added function for colllecting ssd poster metrics, added .out and .sh files to the gitignore

--- adversarial/mains/human_vae_gan_main.py
+++ adversarial/mains/human_vae_gan_main.py
@@ -2,17 +2,20 @@ import torch
 import json
 from adversarial.trainers import human_vae_gan
 
-HPARAMS_PATH = '/active/debruinz_project/jack_lukomski/MMVAE_Adversarial_Team/adversarial/hparams/human_vae_gan.json'
+HPARAMS_PATH = '/active/debruinz_project/cardell_taylor/MMVAE_Adversarial_Team/adversarial/hparams/human_vae_gan.json'
+CSV_PATH = '/active/debruinz_project/cardell_taylor/MMVAE_Adversarial_Team/adversarial/results/loss.csv'
 
 def main(device):
 
     with open(HPARAMS_PATH, mode='r') as file:
         json_hparams = json.load(file)
 
-    hparams = human_vae_gan.HumanVAEConfig(config=json_hparams)
-    trainer = human_vae_gan.HumanVAETrainer(device=device, hparams=hparams)
+    hparams = human_vae_gan.HumanVAEGANConfig(config=json_hparams)
+    trainer = human_vae_gan.HumanVAEGANTrainer(device=device, hparams=hparams)
 
-    trainer.train(100)
+    trainer.train(9)
+
+    trainer.get_run_csv(CSV_PATH)
 
 if __name__ == "__main__":
     CUDA = True
