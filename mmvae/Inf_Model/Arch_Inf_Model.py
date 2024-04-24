018567bdcc06b6eddd44fc5ed36ab14b24bf71ef
Parker S. Bernreuter <bernreup@clipper.cm.cluster>
2024-04-24 18:16:08 -0400

    Added Inference script to notebook and added save to the trainer in notebook

--- mmvae/Inf_Model/Arch_Inf_Model.py
+++ mmvae/Inf_Model/Arch_Inf_Model.py
@@ -1,111 +1,48 @@
 import torch
-import torch.nn as nn
-import mmvae.models.utils as utils
 import numpy as np
-from mmvae.data import MappedCellCensusDataLoader
+from mmvae.data import configure_singlechunk_dataloaders
+import mmvae.models.Arch_Model as Arch_Model
 import time
 import datetime
-import sys
-
-class VAE(nn.Module):
-    def __init__(self):
-        super(VAE, self).__init__()
-        # Encoder
-        self.encoder = nn.Sequential(
-            nn.Linear(60664, 1024),
-            nn.ReLU(),
-            nn.BatchNorm1d(1024, 0.8),
-            nn.Linear(1024, 768),
-            nn.ReLU(),
-            nn.BatchNorm1d(768, 0.8),
-            nn.Linear(768, 256),
-            nn.ReLU(),
-            nn.BatchNorm1d(256, 0.8)
-        )
-        
-        self.fc_mu = nn.Linear(256, 128)
-        self.fc_var = nn.Linear(256, 128)
-        
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(128, 256),
-            nn.ReLU(),
-            nn.BatchNorm1d(256, 0.8),
-            nn.Linear(256, 768),
-            nn.ReLU(),
-            nn.BatchNorm1d(768, 0.8),
-            nn.Linear(768, 1024),
-            nn.ReLU(),
-            nn.BatchNorm1d(1024, 0.8),
-            nn.Linear(1024, 60664),
-            nn.Sigmoid(),
-        )
-
-        utils._submodules_init_weights_xavier_uniform_(self.encoder)
-        utils._submodules_init_weights_xavier_uniform_(self.decoder)
-        utils._submodules_init_weights_xavier_uniform_(self.fc_mu)
-        utils._xavier_uniform_(self.fc_var, -1.0)
-
-
-    def encode(self, x):
-        x = self.encoder(x)
-        return self.fc_mu(x), self.fc_var(x)
-
-    def decode(self, z):
-        return self.decoder(z)
-    
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mu + eps * std
-    
-    def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        recon_x = self.decode(z)
-        return recon_x
-    
-
-input_file = sys.argv[1]
 
 #Create Instance of Device and Data Loader
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-loader =  MappedCellCensusDataLoader(
-    batch_size=512,
-    device=device,
-    file_path= input_file,  #'/actvive/debruinz_project/CellCensus_3M/3m_human_chunk_30.npz',
-    load_all=True,
-    shuffle=False
-)
+loader = configure_singlechunk_dataloaders(
+            data_file_path = '/active/debruinz_project/CellCensus_3M/3m_human_chunk_10.npz',#Path to Data being sent through model
+            metadata_file_path = None,
+            shuffle = False,
+            train_ratio = 1,
+            batch_size = 128,
+            device = None
+        )
 
-#Load Save Model 
-model_path = '/active/debruinz_project/parker_bernreuter/Trained_Model.pth'
+#Load Save Model
+model_path = '/active/debruinz_project/parker_bernreuter/Trained_Model_Full_04-10-24.pth'#Path to file with model
 model_dict = torch.load(model_path, map_location=torch.device(device))
 
 #Create Instance of VAE and load to device
-model = VAE()
+model = Arch_Model.VAE()
 model.to(device)
 
-#Set to Evaluation Mode
+#Load weights and set to Evaluation Mode
 model.load_state_dict(model_dict)
 model.eval()
 
-#Start the timer
-start_time = time.time()
-
-
+#Instantiate dense output matrix
 all_outputs = []
 
+#Loop to send data through model
 with torch.no_grad():
-    for i,x in enumerate(loader):
+    for i,(x, _) in enumerate(loader):
+        #Load data to device and run 
         x = x.to(device)
         output = model(x)
-        output = output.cpu().numpy()
+        output = output[0].cpu().numpy().astype(np.float16) 
         for z in output:
             all_outputs.append(z)
 
 
-dt_object = datetime.datetime.fromtimestamp(start_time)
+#Save the output in file with time as name
+dt_object = datetime.datetime.fromtimestamp(time.time())
 format_time = dt_object.strftime('%Y-%m-%d-%H-%M-%S')
-
 np.savez('/active/debruinz_project/parker_bernreuter/model_outputs/model_output_'+ str(format_time) +'.npz', all_outputs)
