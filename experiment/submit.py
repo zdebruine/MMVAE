0f9cfbcf239725d0b81111a6a030acb6f7f5b37e
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-07-12 16:44:10 -0400

    submit.py added track fixes

--- experiment/submit.py
+++ experiment/submit.py
@@ -6,63 +6,56 @@ import yaml
 
 def submit_job(args):
     subprocess.run(["sbatch", "experiment/submit.sh"] + args)
+
+def parse_value(argkey, value):
+    if isinstance(value, dict):
+        return ' '.join(f"{argkey}.{k} {v}" for k, v in value.items())
+    elif isinstance(value, list):
+        return ' '.join(v for v in value)
+    return f"{argkey} {value}"
+        
+def get_tracked_command_line_options(argkey, config):
+    if len(config) == 1:
+        key, value = next(iter(config.items()))
+        if not isinstance(value, list):
+            return [(argkey, key, value)]
+        return [(argkey, f"{key}_{v}", v) for v in value]
+    else:
+        return [(argkey, key, value) for key, value in config.items()]
+        
+def get_lightning_fit_args_combos(config: dict[str, Any]):
     
-def generate_combinations(config):
     combinations = []
-    for basekey, val in config.items():
-        key = f"{'-' if len(basekey) == 1 else '--'}{basekey}"
-        if isinstance(val, list):
-            combinations.append([(key, "", v) for v in val])
-        elif isinstance(val, dict):
-            if 'track' in val:
-                for tk, tv in val['track'].items():
-                    if isinstance(tv, list):
-                        combinations.append([(key, f"{tk}_{v}", v) for v in tv])
-                    else:
-                        combinations.append([(key, tk, tv)])
-            else:
-                combinations.append([(key, "", val)])
+    for key, value in config.items():
+        argkey = f"-{key}" if len(key) == 1 else f"--{key}"
+        
+        if value == 'flag':
+            combinations.append([(argkey, "", "")])
+            continue
+        
+        if isinstance(value, dict) and 'track' in value:
+            combinations.append(get_tracked_command_line_options(argkey, value['track']))
         else:
-            combinations.append([(key, "", val)])
+            combinations.append([(argkey, "", value)])
             
     combinations = list(itertools.product(*combinations))
     return combinations
-    
-def submit_main(config: dict[str, Any]):
-    """Submit combinations of experiments to sbatch. Combinations are created by specifiying
-    kwargs that are supported as a value, list, or dict. Values will be the same for every run.
-    Lists will be turned into combinations of each value and dictionarys will be the same for their values
-    the only difference is the keys for the dictionary will specifify the nameing value for the run name.
-    (ie. config_path is long so can be shortened with dictionary keys)
-    """
-    
-    jobs = generate_combinations(config['lightning_fit_args'])
 
-    snakemake_args = []
-    snakemake_config_args = []
+def get_snakemake_config_args(config: dict[str, Any]):
+    args = []
     for key, value in config.items():
         if key in ('lightning_fit_args', 'run_name'):
             continue
-        if '.flag' in key:
-            argkey = f"--{key.removesuffix('.flag')}" 
-            snakemake_args.append(f"{argkey} {value}" if value else argkey)
-        else:
-            snakemake_config_args.append(f"{key}={value}")
-            
-    run_name = config.get('run_name', '')
-    
-    for job in jobs:
-        
-        lightning_fit_args = " ".join([f"{key} {value}" for key, _, value in job])
-        
-        if not run_name:
-            _run_name = "_".join([name for _, name, _, in job if name])
-        else:
-            _run_name = run_name
-        
-        args = ['--config', f"lightning_fit_args=\"{lightning_fit_args}\"", f"run_name={_run_name}", *snakemake_config_args, *snakemake_args, ]
+        args.append(f"{key}={value}")
+    return args
 
-        submit_job(args)
+def get_snakemake_args(config):
+    args = []
+    for key, value in config.items():
+        argkey = f"-{key}" if len(key) == 1 else f"--{key}"
+        if value == 'flag':
+            args.append(argkey)
+    return args
 
 if __name__ == "__main__":
     import argparse 
@@ -80,4 +73,12 @@ if __name__ == "__main__":
     if not config:
         raise RuntimeError("Config invalid yaml file or empty!")
             
-    submit_main(config)
+    # submit_main(config)
+    jobs = get_lightning_fit_args_combos(config['lightning_fit_args'])
+    snakemake_config_args = get_snakemake_config_args(config)
+    snakemake_args = get_snakemake_args(config)
+    
+    for job in jobs:
+        lightning_fit_args = ' '.join(parse_value(argkey, value) for argkey, _, value in job)
+        args = ['--config', f"lightning_fit_args=\"{lightning_fit_args}\"", f"run_name=a", *snakemake_config_args, *snakemake_args, ]
+        submit_job(args)
