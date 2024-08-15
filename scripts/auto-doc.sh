dc988e86fc7ab7387cb6028925732b2f61856c7b
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-08-15 16:41:11 -0400

    removed summer_merge, changed docs output to .cmmvae/docs and only for main

--- scripts/auto-doc.sh
+++ scripts/auto-doc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pdoc ./cmmvae --output docs --docformat google --show-source --no-include-undocumented -e cmmvae=https://github.com/zdebruine/MMVAE/
+pdoc ./cmmvae --output ./.cmmvae/docs --docformat google --show-source --no-include-undocumented -e cmmvae=https://github.com/zdebruine/MMVAE/
