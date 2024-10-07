d0a9b41453c7e14fad5a07d6fa124ccafe4ba7cc
Tony Boos <anthonyboos559@gmail.com>
2024-10-07 17:05:00 -0400

    Updates stat functions to be per-category

--- scripts/data-preprocessing/census_data_stats.py
+++ scripts/data-preprocessing/census_data_stats.py
@@ -1,53 +1,85 @@
 import glob
 import os
 import argparse as ap
+from collections import defaultdict
+
 import pandas as pd
 import scipy.sparse as sp
 
-from typing import Union
-from collections import defaultdict
 from data_processing_functions import extract_file_number, gather_stats, record_stats, DATA_CATEGORIES
 
-def update_totals(totals: dict[str, dict[str, int]], data: dict[str, Union[int, dict[str, int]]]):
-    
-    for category in DATA_CATEGORIES:
-        for key, value in data[category]:
-            totals[category][key] += value
-
 def main(directory: os.PathLike, species: str):
+    """
+    Main function for gathering stats on downloaded CellxGene Census data.
 
-    data_files = glob.glob(os.path.join(directory, f'{species}*.npz'))
-    metadata_files = glob.glob(os.path.join(directory, f'{species}*.pkl'))
+    The mean and standard deviation for each file are recorded to a csv.
+    The counts for each unique value for each type of data specified in 
+    DATA_CATEGORIES is also recorded per-file alongside total counts.
+
+    The is meant as a sanity check on randomly sampled subsets of the
+    full data and is not intended to gather stats on all available 
+    CellxGene census data.
+
+    Args:
+        directory (PathLike):
+            The directory holding the downloaded data.
+        species (str):
+            The name of the species whose data is being checked. This
+            is used to perform pattern matching in the directory.
+    """
+    data_files = glob.glob(os.path.join(directory, f"{species}*.npz"))
+    metadata_files = glob.glob(os.path.join(directory, f"{species}*.pkl"))
+
+    # Sort by file number rather than filename
     data_files.sort(key=extract_file_number)
     metadata_files.sort(key=extract_file_number)
 
-    totals = {
-        cat: defaultdict(int) for cat in DATA_CATEGORIES
+    # Storage for data on all files
+    count_stats = defaultdict(dict)
+    total_stats = {
+        category: defaultdict(lambda: defaultdict(int))
+        for category in DATA_CATEGORIES
     }
+    filenames = []
 
     for data_path, metadata_path in zip(data_files, metadata_files):
+
+        file = os.path.basename(data_path).split(".")[0]
+        filenames.append(file)
+        
         data = sp.load_npz(data_path)
         metadata = pd.read_pickle(metadata_path)
-
+        
         chunk_stats = gather_stats(data, metadata)
-        filename = f'{os.path.basename(data_path).split(".")[0]}_stats.csv'
 
-        update_totals(totals, chunk_stats)
+        count_stats["mean"][file] = chunk_stats["mean"]
+        count_stats["std"][file] = chunk_stats["std"]
 
-        record_stats(
-            path= os.path.join(directory, filename),
-            data= chunk_stats
-        )
+        # Update the total counts with the current files data
+        for category in DATA_CATEGORIES:
+            for value, count in chunk_stats[category].items():
+                total_stats[category][value][file] = count
 
-    record_stats(
-        path=os.path.join(directory, f'{species}_stat_totals.csv'),
-        data=totals
+    features_df = pd.DataFrame(count_stats)
+    features_df.to_csv(
+        os.path.join(directory,
+        f"{species}_data_distribution.csv")
     )
 
-if __name__ == '__main__':
+    for category in DATA_CATEGORIES:
+        df = pd.DataFrame(data[category].T.fillna(0))
+        df.columns = filenames
+        df["Total"] = df.sum(axis=1)
+        df.to_csv(os.path.join(directory, f"{species}_{category}_distribution.csv"))
+
+if __name__ == "__main__":
     parser = ap.ArgumentParser()
-    parser.add_argument("--directory", type=str, required=True, help="Directory to load data from.")
-    parser.add_argument("--species", type=str, required=True, help="Species to check data for.")
+    parser.add_argument(
+        "--directory", type=str, required=True, help="Directory to load data from."
+    )
+    parser.add_argument(
+        "--species", type=str, required=True, help="Species to check data for."
+    )
 
     args = parser.parse_args()
     main(
--- scripts/data-preprocessing/data_processing_functions.py
+++ scripts/data-preprocessing/data_processing_functions.py
@@ -2,15 +2,25 @@ import csv
 import glob
 import os
 import re
+
 import numpy as np
 import pandas as pd
 import scipy.sparse as sp
 
-from typing import Union
+DATA_CATEGORIES = ["assay", "cell_type", "tissue"]
 
-DATA_CATEGORIES = ['assay', 'cell_type', 'tissue']
+def normalize_data(
+    data: sp.csr_matrix,
+):
+    """
+    Function to perform normalization on the RAW counts. Each row is summed,
+    and then every value in the row is multiplied by 1e4 before being divided
+    by the row sum. These values are passed to the standard log1p function.
 
-def normalize_data(data: sp.csr_matrix):
+    Args:
+        data (scipy.sparse.csr_matrix):
+            The data to normalize. Normalization happens in-place!
+    """
     for row in range(data.shape[0]):
         start_idx = data.indptr[row]
         end_idx = data.indptr[row + 1]
@@ -18,31 +28,33 @@ def normalize_data(data: sp.csr_matrix):
         row_data = data.getrow(row).data
         row_sum = row_data.sum()
 
-        # Apply the transformation and log1p
         data.data[start_idx:end_idx] = np.log1p((row_data * 1e4) / row_sum)
 
-
-def save_data_to_disk(data_path: os.PathLike, metadata_path: os.PathLike, data: sp.csr_matrix, metdata: pd.DataFrame):
+def save_data_to_disk(
+    data_path: os.PathLike,
+    metadata_path: os.PathLike,
+    data: sp.csr_matrix,
+    metdata: pd.DataFrame,
+):
+    """
+    Helper function to save a pair of count data and its associated
+    metadata to disk at the provided paths.
+    """
     sp.save_npz(data_path, data)
     metdata.to_pickle(metadata_path, compression=None)
 
-def record_stats(path: os.PathLike, data: dict[str, Union[int, dict[str, int]]]):
-    
-    with open(path, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Stat', 'Value(s)'])
-
-        for key, value in data.items():
-            if isinstance(value, dict):
-                merged = ', '.join([f'{k} - {v}' for k, v in value.items()])
-            else:
-                writer.writerow([key, value])
-
 def get_data_stats(data: sp.csr_matrix):
+    """
+    Helper function to get the mean and std of each file.
+    """
     row_sums = data.sum(axis=1).A.flatten()
     return row_sums.mean(), np.std(row_sums)
 
 def gather_stats(data: sp.csr_matrix, metadata: pd.DataFrame):
+    """
+    Gathers counts of unique values for each metadata category specified
+    in DATA_CATEGORIES. Also gets the mean and std of the feature expressions.
+    """
     chunk_stats = {}
     mean, std = get_data_stats(data)
     chunk_stats["mean"] = mean
@@ -54,13 +66,43 @@ def gather_stats(data: sp.csr_matrix, metadata: pd.DataFrame):
     return chunk_stats
 
 def extract_file_number(filename):
-        match = re.search(r'_(\d+)', filename)  # Look for digits after an underscore (_)
-        return int(match.group(1)) if match else 0 
+    """
+    Helper function to sort filenames by their file number.
+    """
+    match = re.search(r"_(\d+)", filename)
+    return int(match.group(1)) if match else 0 
 
-def verify_data(directory: os.PathLike, species: str, ids: set[int], expected_size: int, last_chunk: int = None, last_size: int = None):
-    
-    data_files = glob.glob(os.path.join(directory, f'{species}*.npz'))
-    metadata_files = glob.glob(os.path.join(directory, f'{species}*.pkl'))
+def verify_data(
+    directory: os.PathLike,
+    species: str,
+    ids: set[int],
+    expected_size: int,
+    last_chunk: int = None,
+    last_size: int = None,
+):
+    """
+    Checks that all data 'chunks' are of the expected size and that they match
+    their associated metadata. It also checks that no duplicate data exists.
+
+    Args:
+        directory (PathLike):
+            The directory holding the downloaded data.
+        species (str):
+            The name of the species whose data is being checked. This
+            is used to perform pattern matching in the directory.
+        ids (set[int]):
+            Set containing all expected soma_joinids. Used to ensure no
+            duplicate data is found in a chunk.
+        expected_size (int):
+            The expected number of samples in the data and metadata.
+        last_chunk (int, optional):
+            The final chunk to be checked. Used to signal that the last
+            chunk has a different sample size from the rest.
+        last_size (int, optional):
+            The expected number of samples in the last chunk.
+    """
+    data_files = glob.glob(os.path.join(directory, f"{species}*.npz"))
+    metadata_files = glob.glob(os.path.join(directory, f"{species}*.pkl"))
     data_files.sort(key=extract_file_number)
     metadata_files.sort(key=extract_file_number)
 
@@ -78,7 +120,7 @@ def verify_data(directory: os.PathLike, species: str, ids: set[int], expected_si
                 print(f"Chunk size mismatch in chunk #{i}!!!!!")
                 errors_detected = True
         
-        soma_ids = list(metadata['soma_joinid'])
+        soma_ids = list(metadata["soma_joinid"])
         num_ids = len(soma_ids)
         if num_ids != data.shape[0]:
             print(f"Metadata mismatch in chunk #{i}!!!!!")
@@ -86,7 +128,7 @@ def verify_data(directory: os.PathLike, species: str, ids: set[int], expected_si
 
         set_ids = set(soma_ids)
         if len(set_ids) != num_ids:
-            print(f'Duplicate IDs found in chunk #{i}!!!!!')
+            print(f"Duplicate IDs found in chunk #{i}!!!!!")
             errors_detected = True
 
         ids.intersection_update(set_ids)
