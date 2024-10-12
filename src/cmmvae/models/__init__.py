0ecbed03575b7e4669304bb48d876853df9e47da
Jack Foreback <forebacj@mail.gvsu.edu>
2024-10-12 16:40:47 -0400

    Added MOE components to __init__ files in cmmvae.model and cmmve.modules

--- src/cmmvae/models/__init__.py
+++ src/cmmvae/models/__init__.py
@@ -3,9 +3,11 @@
 """
 from cmmvae.models.base_model import BaseModel
 from cmmvae.models.cmmvae_model import CMMVAEModel
+from cmmvae.models.moe_cmmvae_model import MOE_CMMVAEModel
 
 
 __all__ = [
     "BaseModel",
     "CMMVAEModel",
+    "MOE_CMMVAEModel",
 ]
