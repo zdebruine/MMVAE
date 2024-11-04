32df3962695a85693dfbf4dbe53acce68ead09b7
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-11-04 15:13:09 -0500

    added version to run-compare

--- scripts/run-compare.sh
+++ scripts/run-compare.sh
@@ -10,20 +10,24 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-for file in "$2"/*.yaml
-do
-    filename=$(basename "$file" .yaml)
-    echo "Processing: $filename"
-    sbatch scripts/run-snakemake.sh --config \
-        experiment_name=$1\
-        run_name=$filename \
-        root_dir=lightning_logs \
-        train_command=\
-"\
-fit \
---data configs/data/local.yaml \
---model $file \
---trainer.max_epochs 5 \
-"
+experiment_name=$1
+compare_dir=$2
 
+shift 2
+
+root_dir=lightning_logs
+data=configs/data/local.yaml
+max_epochs="--trainer.max_epochs 5"
+
+for file in $compare_dir/*.yaml
+do
+  run_name=$(basename "$file" .yaml)
+  version=$(ls -d "$root_dir/$experiment_name/$base_run_name"* | grep -E 'V[0-9]{3}$' | sort -V | tail -n 1 | sed -E 's/V([0-9]{3})$/\1/' | awk '{printf "V%03d", $1 + 1}')
+
+  echo "Processing: $file"
+  sbatch scripts/run-snakemake.sh --config \
+    root_dir="$root_dir" \
+    experiment_name="$experiment_name" \
+    run_name="${run_name}.${version}" \
+    train_command="fit --data $data --model $file $max_epochs $*"
 done
