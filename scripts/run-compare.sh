55a7146289772cb35e07814d215cd51c29ce9a83
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-11-04 15:58:26 -0500

    updated run-compare to take command line args

--- scripts/run-compare.sh
+++ scripts/run-compare.sh
@@ -1,33 +1,120 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "No argument provided for the name of the experiment!"
+debug=false
+append_commit_hash=true
+root_dir="${CMMVAE_ROOT_DIR}"
+experiment="${CMMVAE_EXPERIMENT_NAME}"
+data="${CMMVAE_DATA_CONFIG}"
+compare=""
+max_epochs="${CMMVAE_MAX_EPOCHS}"
+commit_hash=""
+
+if [ -z "${max_epochs}" ]; then
+  max_epochs=5
+else
+  echo "CMMVAE_MAX_EPOCHS is set to '$CMMVAE_MAX_EPOCHS'"
+fi
+
+if [ -z "${data}" ]; then
+  data=configs/data/local.yaml
+else
+  echo "CMMVAE_DATA_CONFIG is set to '$CMMVAE_DATA_CONFIG'"
+fi
+
+if [ -z "${root_dir}" ]; then
+  root_dir=lightning_logs
+else
+  echo "CMMVAE_ROOT_DIR is set to '$CMMVAE_ROOT_DIR'"
+fi
+
+if [ -z "${experiment}" ]; then
+  experiment=default
+else
+  echo "CMMVAE_EXPERIMENT_NAME is set to '$CMMVAE_EXPERIMENT_NAME'"
+fi
+
+for arg in "$@"
+do
+    case $arg in
+        --debug)
+            debug=true
+            shift
+            ;;
+        --no-commit-hash)
+            append_commit_hash=false
+            shift
+            ;;
+        root_dir=*)
+            root_dir="${arg#*=}"
+            shift
+            ;;
+        experiment=*)
+            experiment="${arg#*=}"
+            shift
+            ;;
+        compare=*)
+            compare="${arg#*=}"
+            shift
+            ;;
+        data=*)
+            data="${arg#*=}"
+            shift
+            ;;
+        max_epochs=*)
+            max_epochs="${arg#*=}"
+            shift
+            ;;
+        # *)
+        #     echo "Unknown argument: $arg"
+        #     ;;
+
+    esac
+done
+
+if [ -z "$compare" ]; then
+  echo "Please specify directory that contains model configs to compare."
   exit 1
 fi
 
-if [ -z "$2" ]; then
-  echo "No argument provided for model config filename!"
-  exit 1
+if [ "$append_commit_hash" = true ]; then
+  # Check if Git is installed
+  if ! command -v git &> /dev/null; then
+      echo "Error: Git is not installed. Please install Git to use this script or specify --no-commit-hash."
+      exit 1
+  fi
+
+  # Check if inside a Git repository
+  if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+      echo "Error: This is not a Git repository. Please run the script inside a Git repository or specify --no-commit-hash."
+      exit 1
+  fi
+
+  # Fetch and show the latest commit hash
+  commit_hash=$(git rev-parse --short HEAD)
+  echo "Latest Commit Hash: $commit_hash"
+else
+  echo "Skipping commit hash display."
 fi
 
-experiment_name=$1
-compare_dir=$2
-
-shift 2
-
-root_dir=lightning_logs
-data=configs/data/local.yaml
-max_epochs="--trainer.max_epochs 5"
-
-for file in $compare_dir/*.yaml
+for file in $compare/*.yaml
 do
   run_name=$(basename "$file" .yaml)
-  version=$(ls -d "$root_dir/$experiment_name/$base_run_name"* | grep -E 'V[0-9]{3}$' | sort -V | tail -n 1 | sed -E 's/V([0-9]{3})$/\1/' | awk '{printf "V%03d", $1 + 1}')
+
+  if [ "$commit_hash" != "" ]; then
+    run_name="${run_name}.${commit_hash}"
+  fi
+
+  version=$(find "$root_dir/$experiment" -maxdepth 1 -mindepth 1 -type d -printf '%f\n' | grep -E 'V[0-9]{3}$' | sort -V | tail -n 1 | sed -E 's/V([0-9]{3})$/\1/' | awk '{printf "V%03d", $1 + 1}')
 
   echo "Processing: $file"
-  sbatch scripts/run-snakemake.sh --config \
-    root_dir="$root_dir" \
-    experiment_name="$experiment_name" \
-    run_name="${run_name}.${version}" \
-    train_command="fit --data $data --model $file $max_epochs $*"
+  command="scripts/run-snakemake.sh --config \
+    root_dir=${root_dir} \
+    experiment_name=${experiment} \
+    run_name=${run_name}.${version} \
+    train_command=\"fit --data $data --model $file --trainer.max_epochs $max_epochs $*\"
+    "
+  echo $command
+  if [ "$debug" = false ]; then
+    sbatch $command
+  fi
 done
