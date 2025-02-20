b3cf5ce5c7eecf93bb1289e3a32fc40ade77e879
Tony Boos <anthonyboos559@gmail.com>
2025-02-20 12:29:19 -0500

    Makes cross-generation importable and adds test example of using it to get correlations

--- src/cmmvae/runners/workflow.py
+++ src/cmmvae/runners/workflow.py
@@ -2,8 +2,6 @@ import click
 
 from cmmvae.runners.expression import expression
 from cmmvae.runners.cli import cli
-from cmmvae.runners.correlations import correlations
-from cmmvae.runners.run_correlations import run_correlations
 from cmmvae.runners.umap_predictions import umap_predictions
 from cmmvae.runners.merge_predictions import merge_predictions
 from cmmvae.runners.meta_discriminators import meta_discriminator
@@ -16,8 +14,6 @@ def workflow():
 
 workflow.add_command(expression)
 workflow.add_command(cli)
-workflow.add_command(correlations)
-workflow.add_command(run_correlations)
 workflow.add_command(umap_predictions)
 workflow.add_command(merge_predictions)
 workflow.add_command(meta_discriminator)
